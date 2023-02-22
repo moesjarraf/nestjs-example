00de64e51a1eb77bddfb4b20b523be23bae45394
moesjarraf <moesjarraf@gmail.com>
2023-02-22 19:21:30 +0100

    use common library and format project

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -8,4 +8,4 @@ import { ConfigService } from './config.service';
   providers: [ConfigService],
   exports: [ConfigService],
 })
-export class ConfigModule { }
+export class ConfigModule {}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,11 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModuleConfig, DatabaseService, DB_DEFAULT_CONNECTION } from '@moesjarraf/nestjs-common';
+import {
+  AppModuleConfig,
+  DatabaseService,
+  DB_DEFAULT_CONNECTION,
+} from '@moesjarraf/nestjs-common';
 
 describe('Application e2e test', () => {
   let app: INestApplication;
@@ -9,9 +13,14 @@ describe('Application e2e test', () => {
   beforeAll(async () => {
     const module = await Test.createTestingModule(AppModuleConfig)
       .overrideProvider(DB_DEFAULT_CONNECTION)
-      .useFactory({ factory: (db: DatabaseService) => db.connect(), inject: [DatabaseService] })
+      .useFactory({
+        factory: (db: DatabaseService) => db.connect(),
+        inject: [DatabaseService],
+      })
       .compile();
-    app = module.createNestApplication(undefined, { logger: ['error', 'warn'] });
+    app = module.createNestApplication(undefined, {
+      logger: ['error', 'warn'],
+    });
     await app.init();
   }, 30000);
 
